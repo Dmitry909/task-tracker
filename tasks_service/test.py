1e5c51d2b258d49e46f2e56e28f70a0dbf80045b
Dmitry07 <dmitry.terenich@gmail.com>
2024-06-07 14:24:20 +0300

    Add test

---
+++ tasks_service/test.py
@@ -0,0 +1,42 @@
+from test_common import *
+import random
+from string import ascii_lowercase, digits
+
+
+def random_str(length):
+    return ''.join(random.choice(ascii_lowercase + digits) for _ in range(length))
+
+
+def do_everything_test():
+    author_id = random.randint(100, 999)
+    text1 = random_str(10)
+    
+    task_id1 = create_task(author_id, text1).task_id
+    task1 = get_task(author_id, task_id1)
+    assert task1.task_id == task_id1 and task1.author_id == author_id and task1.text == text1
+
+    text2 = random_str(10)
+    update_task(author_id, task_id1, text2)
+    task2 = get_task(author_id, task_id1)
+    assert task2.task_id == task_id1 and task2.author_id == author_id and task2.text == text2
+
+    text3 = random_str(10)
+    task_id3 = create_task(author_id, text3).task_id
+    text4 = random_str(10)
+    task_id4 = create_task(author_id, text4).task_id
+    text5 = random_str(10)
+    task_id5 = create_task(author_id, text5).task_id
+
+    list_response = list_tasks(author_id, 0, 100)
+    ids = [task.task_id for task in list_response.tasks]
+    assert ids == [task_id1, task_id3, task_id4, task_id5]
+
+    delete_task(author_id, task_id4)
+    list_response = list_tasks(author_id, 0, 100)
+    ids = [task.task_id for task in list_response.tasks]
+    assert ids == [task_id1, task_id3, task_id5]
+
+    print('do_everything_test OK')
+
+
+do_everything_test()
---
+++ tasks_service/test_common.py
@@ -0,0 +1,33 @@
+import grpc
+import tasks_pb2
+import tasks_pb2_grpc
+import json
+
+host = 'localhost:23456'
+channel = grpc.insecure_channel(host)
+stub = tasks_pb2_grpc.TaskServiceStub(channel)
+
+
+def create_task(author_id, text):
+    request = tasks_pb2.CreateTaskRequest(author_id=author_id, text=text)
+    return stub.CreateTask(request)
+
+
+def update_task(user_id, task_id, new_text):
+    request = tasks_pb2.UpdateTaskRequest(user_id=user_id, task_id=task_id, new_text=new_text)
+    return stub.UpdateTask(request)
+
+
+def delete_task(user_id, task_id):
+    request = tasks_pb2.DeleteTaskRequest(user_id=user_id, task_id=task_id)
+    return stub.DeleteTask(request)
+
+
+def get_task(user_id, task_id):
+    request = tasks_pb2.GetTaskRequest(user_id=user_id, task_id=task_id)
+    return stub.GetTask(request)
+
+
+def list_tasks(user_id, offset, limit):
+    request = tasks_pb2.ListTasksRequest(user_id=user_id, offset=offset, limit=limit)
+    return stub.ListTasks(request)
